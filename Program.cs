afc750936297bc0caa5d9a4b9609d1142b63e931
Natalia <natusik_kem@list.ru>
2022-08-24 00:16:48 +0300

    DZ349

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+       if(i<array.Length-1)Console.Write($"{array[i]}, ");
+       else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
+int[] GetSummElem(int[] array)
+{
+int SumElem = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0 ) SumElem ++;
+  }
+return new int[] {SumElem};
+}
+
+int[] arr = CreateArrayRndInt(4, 100, 999);
+
+PrintArray(arr);
+
+int[] result = GetSummElem(arr);
+Console.WriteLine ($"Количество четных элементов: {result[0]}");
+
+
+
+
